9aac849bb70f2c7e15c7adea72873c8ac5128f3a
Rakamishu <mkrotify@gmail.com>
2017-09-23 01:44:20 +0300

    redakciq na komentar

--- PHP_BASICS_SYNTAX-Lab/Problem_2_Render_students_info.php
+++ PHP_BASICS_SYNTAX-Lab/Problem_2_Render_students_info.php
@@ -52,7 +52,7 @@ if(isset($_SESSION['names'], $_SESSION['ages'], $_SESSION['age_restriction']))
     $age_restriction = $_SESSION['age_restriction']; // 4ast 3 ot zada4ata - Filter Legal Students
     $getpage = isset($_GET['page']) ? $_GET['page'] : 1; //vzima teku6tata stranica ili ako ne e setnata slaga po default 1
     
-    $list = []; //sazdavame si nov masiv v koito 6te vkarvame otdelen masiv ot vseki potrebiteli i negovite imena i godini
+    $list = []; //sazdavame si nov masiv v koito 6te vkarvame otdelen masiv za vseki potrebitel i negovite imena i godini
     
     for($i = 0; $i < count($_SESSION['names']); $i++)
     {
